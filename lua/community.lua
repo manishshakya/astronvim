9469ec7047db03f5c3e46620f8df4a7490a40779
Manish Shakya <msh.shakya@gmail.com>
2025-04-19 12:06:49 -0400

    list of plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,5 +7,18 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim"},
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim"},
+  { import = "astrocommunity.note-taking.zk-nvim"},
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" }, -- import/override with your plugins folder
 }
